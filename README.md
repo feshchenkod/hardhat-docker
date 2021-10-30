f1ae103792f5dd38d9091dd89dcfa99e107ac488
_den <_den@outlook.com>
2021-10-30 17:54:05 +1000

    upload files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # hardhat-docker
+
+```
+docker build -t hardhat-node .
+docker-compose up -d
+docker volume create hardhat-cache
+./fork.sh --help
+```
---
+++ hardhat/fork.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+source .env
+CACHE="-v hardhat-cache:/app/cache"
+TIMEOUT=2h
+HEIGHT=
+NAME=`uuidgen`
+
+print_help () {
+  echo
+  echo "options:"
+  echo "-b     Fork from block number (Default: latest, minus safety value - 5). Should be 2675000 at least."
+  echo "-h     Print this Help."
+  echo "-n     Disable cache loading."  
+  echo "-t     Custom timeout (default: 2h)."
+  echo
+  echo "example: ./fork.sh -n -b 2675000 -t 30m"
+  echo
+  exit 0
+}
+
+while getopts 'nb:ht:' flag; do
+  case "${flag}" in
+    n) CACHE= ;;
+    b) HEIGHT="--fork-block-number ${OPTARG}";;
+    h) print_help ;;
+    t) TIMEOUT="${OPTARG}" ;;
+    *) print_help
+       exit 1 ;;
+  esac
+done
+
+docker run -d \
+--network hardhat_default $CACHE \
+-l "traefik.enable=true" \
+-l "traefik.http.routers.$NAME.service=$NAME" \
+-l "traefik.http.routers.$NAME.rule=Host(\`${DOMAIN}\`) && Path(\`/$NAME/\`)" \
+-l "traefik.http.routers.$NAME.entrypoints=websecure" \
+-l "traefik.http.routers.$NAME.tls.certresolver=myresolver" \
+-l "traefik.http.services.$NAME.loadbalancer.server.port=8545" \
+--name $NAME hardhat-node npx hardhat node \
+--fork $DEFAULT_RPC $HEIGHT \
+1> /dev/null
+
+nohup sleep $TIMEOUT &>/dev/null && docker stop $NAME &>/dev/null && docker rm $NAME &>/dev/null &
+
+echo
+echo "=======> https://${DOMAIN}/$NAME/ <======="
+
+echo
+echo "Auto remove after: $TIMEOUT. To manual remove:"
+echo "=======> docker stop $NAME && docker rm $NAME <======="
+echo
